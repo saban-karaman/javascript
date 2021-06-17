7db03ff45850cb8cf3e9f864a3ddee67d7f9c2bb
Saban Karaman <karaman.saban@gmail.com>
2021-06-17 20:13:30 +0300

    in class ex

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello From js')
+
+
+// console.log('Hello From js');
+// console.info('some info');
+// console.warn('some warning');
+// console.error('error !!!');
+
+// console.log('----------');
+// var myName = 'Matthew';
+// console.log(myName);
+// var announce = "Welcome to our school"
+// console.log(announce)
+
+// const PI = 3.141521;
+
+// let myNum = 5;
+// console.log(PI);
+
+// myNum = PI * 4
+// console.log(myNum);
+
+var num1 = 42;
+console.log(num1, num2);
+var num2 = 88;
+console.log(num1, num2);

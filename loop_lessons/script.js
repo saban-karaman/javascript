b7ae1544395e4c6cbac90d48cc0af351800e0eb5
Saban Karaman <karaman.saban@gmail.com>
2021-06-28 22:42:53 +0300

    in class exercise

---
+++ loop_lessons/script.js
@@ -0,0 +1,38 @@
+// While Loops
+// let i = 0, sum = 0;
+
+// while (i < 6) {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log("The total is:" + sum);
+
+//DO WHILE
+// let i = 5, sum = 0;
+
+// do{
+//     sum += i;
+// } while (i<5) {
+//     console.log(sum);
+// }
+//For Loops
+// for (let i = 0; i < 8; i++) {
+//     console.log("i", i);
+// }
+
+// let brand = "Clarusway";
+// for (let i = 0; i < brand.length; i++){
+//     console.log(i, brand[i]);
+     
+    
+// }
+// let brand = "Clarusway";
+// for (let i = 0; i <= brand.length; i++){
+//     console.log(brand[brand.length - (i+1)]);
+// }
+let x = 9;
+
+for (let i = 1; i <= 9; i++) {
+    console.log(1,"x",i+1,"=",1*(i+1));
+    
+}

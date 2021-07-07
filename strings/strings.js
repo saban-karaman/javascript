4d9339d730a05034f352c2ccc1e60706aa1da8ec
Saban Karaman <karaman.saban@gmail.com>
2021-07-07 22:03:37 +0300

    class exercise

--- strings/strings.js
+++ strings/strings.js
@@ -1,20 +1,48 @@
-var str1 = "Hello";
-var str2 = "world";
-var str3 = "Hello world";
-var str4 = new String("A string object")
+// var str1 = "Hello";
+// var str2 = "world";
+// var str3 = "Hello world";
+// var str4 = new String("A string object")
 
-console.log(typeof str1)
-console.log(typeof str2)
-console.log(typeof str3)
-console.log(typeof str4)
+// console.log(typeof str1)
+// console.log(typeof str2)
+// console.log(typeof str3)
+// console.log(typeof str4)
 
-console.log(str4)
-console.log(str3)
+// console.log(str4)
+// console.log(str3)
 
-console.log("Hello world".length)
-console.log(str2.length)
-console.log(str3.length)
-console.log(str4.length)
+// console.log("Hello world".length)
+// console.log(str2.length)
+// console.log(str3.length)
+// console.log(str4.length)
 
-console.log(str1.concat(str2))
-console.log(str1+str2)
+// console.log(str1.concat(str2))
+// console.log(str1+str2)
+
+//includes
+
+// var str = "lorem ipsum is simply";
+// console.log(str.includes('simply'))
+// console.log(str.includes('Simply'))
+
+//indexOf
+
+// var str = "lorem ipsum is simply";
+// var n1 = str.indexOf("simply");
+// var n = str.indexOf("xx");
+// console.log(n1);
+
+// lastindexoff
+
+// var str = "lorem ipsum is simply";
+// console.log(str.lastIndexOf("l"));
+
+
+//slice()
+// var str = "Lorem Ipsum is simply dummy text of simply printing and typesetting industry.";
+// console.log(str.slice(13,21));
+// console.log(str.slice(13)); 
+
+//substr()
+// var str = "Lorem Ipsum is simply dummy text of simply printing and typesetting industry.";
+// console.log(str.substr(26,10));
